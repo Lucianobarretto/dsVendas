4342e31439262f85a725361d8469dac280b9fa14
Luciano Barreto <contato.lucianobarreto@gmail.com>
2021-09-07 20:16:12 -0300

    Project created, front-end & back-end

---
+++ backend/src/test/java/com/lucianobarreto/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucianobarreto.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
