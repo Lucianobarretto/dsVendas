6237722f0091de89f730ce162ab16970f778a673
Luciano Barreto <contato.lucianobarreto@gmail.com>
2021-09-12 03:10:32 -0300

    First request

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -8,7 +8,7 @@ const Home = () => {
       <NavBar />
       <div className="container">
         <div className="jumbotron">
-          <h1 className="display-3">DSVendas</h1>
+          <h1 className="display-3">LBVendas</h1>
           <p className="lead">
             Analise o desempenho das suas vendas por diferentes perspectivas
           </p>
